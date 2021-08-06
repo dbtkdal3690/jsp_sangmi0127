a64b374b510d7fa319c119422c1e11199a6412a6
dbtkdal3690 <dbtkdal1590@gmail.com>
2021-08-06 12:14:51 +0900

    RQ 처리

--- src/main/java/com/jhs/exam/exam2/servlet/DispatcherServlet.java
+++ src/main/java/com/jhs/exam/exam2/servlet/DispatcherServlet.java
@@ -1,7 +1,16 @@
+package com.jhs.exam.exam2.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.jhs.exam.exam2.http.Rq;
+import com.jhs.exam.exam2.http.controller.Controller;
+import com.jhs.exam.exam2.http.controller.UsrArticleController;
 import com.jhs.mysqliutil.MysqlUtil;
 
 @WebServlet("/usr/*")
@@ -13,13 +22,30 @@ public class DispatcherServlet extends HttpServlet {
 			rq.print("올바른 요청이 아닙니다.");
 		}
 
-		rq.println("controllerTypeName : " + rq.getControllerTypeName());
-		rq.println("<br>");
-		rq.println("controllerName : " + rq.getControllerName());
-		rq.println("<br>");
-		rq.println("actionMethodName : " + rq.getActionMethodName());
+		Controller controller = null;
 
-		MysqlUtil.setDBInfo("localhost", "sbsst", "sbs123414", "jsp_board");
-		MysqlUtil.setDevMode(true);
+		switch (rq.getControllerTypeName()) {
+		case "usr":
+			switch (rq.getControllerName()) {
+			case "article":
+				controller = new UsrArticleController();
+				break;
+			}
+
+			break;
+		}
+
+		if (controller != null) {
+			MysqlUtil.setDBInfo("localhost", "sbsst", "sbs123414", "jsp_board");
+			MysqlUtil.setDevMode(true);
+
+			controller.performAction(rq);
+
+			MysqlUtil.closeConnection();
+		}
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
 	}
 }
